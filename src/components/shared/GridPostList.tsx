9af7d72cc77c8f32d130bbfb45d21cb9d4e3fdac
Anas Yousif <Anasyousif182@gmail.com>
2023-11-07 11:43:49 -0500

    first push

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,4 +1,6 @@
+import { useUserContext } from "@/context/AuthContext"
 import { Models } from "appwrite"
+import { Link } from "react-router-dom"
 
 
 type GridPostListProps = {
@@ -6,8 +8,17 @@ type GridPostListProps = {
 }
 
 const GridPostList = ({ posts } : GridPostListProps) => {
+  const { user } = useUserContext();
   return (
-    <div>GridPostList</div>
+    <ul className="grid-container">
+     {posts.map((post) => (
+      <li key={post.$id} className="relative min-w-80 h-80">
+        <Link to={`/posts/${post.$id}`} className="grid-post_link">
+        <img  src={post.imageUrl} className="h-full w-full object-cover"/>
+        </Link>
+      </li>
+     ))}
+    </ul>
   )
 }
 
